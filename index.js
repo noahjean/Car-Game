72062114b1341f3ba1b4023be55706f32bb82357
jatanolu <jacobrjean@gmail.com>
2023-01-20 14:59:02 -0500

    basic func

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+let e = document.body
+console.log(e)
+	e.addEventListener('keydown', arrows)
 
+
+function arrows(){
+	const key = event.key;
+	car = document.getElementById('car')
+	spot = car.style.gridColumn[0]
+	console.log(spot)
+
+	if(key === 'ArrowLeft'){
+		console.log('hi')
+		car.style.gridColumn = 1
+	}
+	if(key === 'ArrowRight'){
+		console.log('bye')
+		car.style.gridColumn = 3
+	}
+}
